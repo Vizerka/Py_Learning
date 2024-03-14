cbe87ae1e27dc30e4509b5a845232ebf9df97b6b
Vizerka <vizerauwu@gmail.com>
2024-03-14 12:21:16 +0100

    Added file with numbers etc

---
+++ Day2/liczby.py
@@ -0,0 +1,27 @@
+liczba = 1 + 2
+print(liczba)
+liczba = liczba + liczba
+print(liczba)
+print(liczba ** liczba)
+duza_liczba = 100_100_100_100
+print(duza_liczba)
+przecinek_liczba = 4 / 3
+print(przecinek_liczba)
+
+x, y, z = 100, 200, 300
+print(x, y, z)
+
+#Stałe zapisujemy używając WIELKICH LITER
+
+LICZBA_STALA = 621374206
+print(LICZBA_STALA)
+
+
+#zmiennoprzecinkowy madness
+print("Dodając 0.1 do 0.2 wychodzi jakaś z dupy czwórka na końcu zer")
+print(0.1 + 0.2)
+
+print(5+3)
+print(12-4)
+print(4*2)
+print(16/2)
