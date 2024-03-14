b58f868aea451b5da44d37a2efcb83ec0ccebb47
Vizerka <vizerauwu@gmail.com>
2024-03-14 12:23:24 +0100

    added commments to liczby.py

--- Day2/liczby.py
+++ Day2/liczby.py
@@ -1,3 +1,4 @@
+#Tutaj są ćwiczonka z liczbami
 liczba = 1 + 2
 print(liczba)
 liczba = liczba + liczba
@@ -8,19 +9,19 @@ print(duza_liczba)
 przecinek_liczba = 4 / 3
 print(przecinek_liczba)
 
+#wiele przypisań do wielu zmiennych
 x, y, z = 100, 200, 300
 print(x, y, z)
 
 #Stałe zapisujemy używając WIELKICH LITER
-
 LICZBA_STALA = 621374206
 print(LICZBA_STALA)
-
 
 #zmiennoprzecinkowy madness
 print("Dodając 0.1 do 0.2 wychodzi jakaś z dupy czwórka na końcu zer")
 print(0.1 + 0.2)
 
+#wyświwtlanie ósemki na różne sposoby matematyczne
 print(5+3)
 print(12-4)
 print(4*2)
