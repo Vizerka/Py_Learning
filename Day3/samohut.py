99deef32139f6800b43bd4dfb20aaf1cd26b3b47
Vizerka <vizerauwu@gmail.com>
2024-06-12 11:29:09 +0200

    fajne te listy

---
+++ Day3/samohut.py
@@ -0,0 +1,19 @@
+#ogólnie to boli mnie trochę gardło
+#cars["bmw", "auudi", "tata", "bemtlej", "bagatata", "lambardziambar"]
+#tu ma być kurwa równa sie 
+cars = ["bmw", "auudi", "tata", "bemtlej", "bagatata", "lambardziambar"]
+print(cars)
+cars.sort()
+print(cars)
+print("Jeszcze raz")
+cars = ["bmw", "auudi", "tata", "bemtlej", "bagatata", "lambardziambar"]
+print(cars)
+cars.sort(reverse=True) #true musibyć z dużej bo inaczej nie działa uwu
+print(cars)
+print("Jeszcze razraz")
+cars = ["bmw", "auudi", "tata", "bemtlej", "bagatata", "lambardziambar"]
+print(f"{sorted(cars)} \n{cars} \n{sorted(cars, reverse=True)}")
+cars.reverse()
+print(cars)
+cars.reverse()
+print(len(cars))
