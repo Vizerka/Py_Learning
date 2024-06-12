78a04cd1c5784f465d8e9eeb4055fb4d09a5a2b3
Vizerka <vizerauwu@gmail.com>
2024-06-12 11:03:13 +0200

    Wracam do nauki pajtona

---
+++ Day3/przypomnionko.py
@@ -0,0 +1,18 @@
+#komentarze robi się tak
+#były f stringi
+name="uwu"
+name2="owo"
+print(f"{name} {name2}")
+print(f"{name.upper()}")
+print(f"{name2.capitalize()}")
+#były też listy
+motomoto = ["wrum", "bzium", "srum"]
+print(motomoto[1].capitalize())
+#moge też dodać element na liste uwu 
+motomoto.insert(3,"siusiak:3")
+print(motomoto)
+motomoto.insert(69,"hehe 69")
+print(motomoto)
+print(motomoto[4])
+motomoto.remove("siusiak:3")
+print(motomoto)
